24f5354d346eff9854af8f39ea9c2ea75d51174e
vilip22 <viliperse22@gmail.com>
2023-02-28 14:57:30 +0100

    Tutorial 1: First OpenGL ES project

---
+++ RedScreen/app/src/main/java/com/vilip22/redscreen/RedScreenRenderer.java
@@ -0,0 +1,27 @@
+package com.vilip22.redscreen;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import static android.opengl.GLES20.*;
+
+public class RedScreenRenderer implements GLSurfaceView.Renderer {
+
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int width, int height) {
+        glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+
+}
